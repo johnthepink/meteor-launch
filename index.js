1cdf5cd26d50a4e2fb18aa2737c5abb8abbf7183
John Pinkerton <jpinkerton88@gmail.com>
2015-11-22 21:19:20 -0500

    process env before

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 "use strict";
 
 const Vorpal = require('vorpal')(),
-      Path = require('path');
+      Path = require('path'),
+      _ = require('underscore');
 
 const Meteor = require('./lib/meteor'),
       Hockey = require('./lib/hockey');
 
 const launchFile = Path.join(process.cwd(), 'launch.json');
 const launchVars = require(launchFile);
+const superEnv = _.extend(env, process.env);
 
 Vorpal
   .command('build', 'Builds the Meteor app in the .build folder')
   .action(function(args) {
-    Meteor.build(launchVars, (result) => {
+    Meteor.build(superEnv, (result) => {
       return
     })
   });
@@ -22,7 +24,7 @@ Vorpal
   .command('hockey', 'Build and deploy to Hockey')
   .action(function(args) {
     Meteor.build(launchVars, (result) => {
-      Hockey.upload(launchVars, (result) => {
+      Hockey.upload(superEnv, (result) => {
         return
       })
     })
