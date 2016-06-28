1eb3aac0e25e96a32f46c72792e706c27bab7a2a
John Pinkerton <jpinkerton88@gmail.com>
2016-06-28 00:39:27 -0400

    add tests for init

--- src/__test/index.js
+++ src/__test/index.js
@@ -1,1 +1,2 @@
 import "./help";
+import "./init";
---
+++ src/__test/init.js
@@ -0,0 +1,44 @@
+/* global describe it beforeEach */
+
+import { join } from "path";
+import { stat } from "fs";
+import { assert } from "chai";
+import { execSync } from "child_process";
+import util from "./util";
+
+beforeEach(() => {
+  util.cleanLaunchFile();
+});
+
+describe("init", () => {
+  it("should error when no launch.json file", () => {
+    const output = execSync("launch build");
+
+    assert.equal(
+      output.toString(),
+      "launch.json not found. Please run: launch init\n"
+    );
+  });
+
+  it("should return launch message", () => {
+    const output = execSync("launch init");
+
+    assert.equal(
+      output.toString(),
+      "launch.json created. Open it and fill out the vars\n"
+    );
+  });
+
+  it("should add launch file", () => {
+    execSync("launch init");
+    const launchFile = join(process.cwd(), "launch.json");
+
+    stat(launchFile, (err) => {
+      if (err) {
+        assert.fail();
+      } else {
+        assert.isOk();
+      }
+    });
+  });
+});
